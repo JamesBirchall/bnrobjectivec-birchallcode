9b35ff89e4edaee19c826b11cc0019342d1f99a5
James Birchall <jamesbirchall@gmail.com>
2015-08-22 20:43:22 +0100

    Added ScratchPadForC
    A project which will be used throughout the book to test ideas not
    specific to project code.

---
+++ ScratchpadForC/ScratchpadForC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ScratchpadForC
+//
+//  Created by James Birchall on 22/08/2015.
+//  Copyright (c) 2015 James Birchall. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
