892e9e0e7f3c34e3d00e91d1c12a47e3b1d9eb5e
James Birchall <jamesbirchall@gmail.com>
2015-08-22 20:54:56 +0100

    First project - a good start
    Somewhere this project started

---
+++ Chapter2/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by James Birchall on 22/08/2015.
+//  Copyright (c) 2015 James Birchall. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    // a single line comment
+    
+    /* a multi
+     line
+     comment */
+    
+    printf("It was the best of times.\n");
+    printf("It was the blurst of times.\n");
+    printf("We had something of a time.\n");
+    
+    return 0;
+}
