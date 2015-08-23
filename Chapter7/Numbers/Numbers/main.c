7495ded125ade060e8e62fcbce478112e0fda44a
James Birchall <jamesbirchall@gmail.com>
2015-08-23 15:50:34 +0100

    Chapter 7 complete
    Data types for numbers including int, float, long, signed, unsigned and
    math library and sodlib for functions like absolute().

---
+++ Chapter7/Numbers/Numbers/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by James Birchall on 23/08/2015.
+//  Copyright (c) 2015 James Birchall. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+
+//    int x = 255;
+//    printf("x is %d.\n", x);
+//    
+//    printf("In octal, x is %o.\n", x);
+//    
+//    printf("In hexidecimal, x is %x.\n", x);
+
+//    printf("11/3.0 = %.2f.\n", 11/ (float)3);
+//    
+//    printf("The absolute value of -5 = %d.\n", abs(-5));
+
+    printf("The sine of 1 radian = %.3f.\n", sin(1));
+    
+    return 0;
+}
